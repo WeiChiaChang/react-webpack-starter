1dd1fded1ebfe6278e1a913e8c06a937e497b622
WeiChiaChang <ioh04019@gmail.com>
2018-05-21 22:38:42 +0800

    ✨ Feat: Webpack plugins & modules updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Kuma from "../images/taiwan-kuma.gif";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <img src={Kuma} alt="" height="50" />
         <h1 className="title">My React-Webpack-Starter</h1>
       </div>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,75 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+let pathsToClean = [
+  'dist',
+]
+
+var cssDev = ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    "app.bundle": './src/index.js'
+  },
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'index-bundle.js'
+    filename: 'app.bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js|jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader'
         }
       },
       {
         test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader' ]
-      }
+        // use: ExtractTextPlugin.extract({
+        //   fallback: 'style-loader',
+        //   //resolve-url-loader may be chained before sass-loader if necessary
+        //   use: ['css-loader', 'sass-loader']
+        // })
+        use: cssDev,
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/,
+      },
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(pathsToClean),
+    new ExtractTextPlugin('style.css'),
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true
     })
-  ]
+  ],
+  devtool: 'source-map',
 }
